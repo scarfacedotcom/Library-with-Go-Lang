a8162ae52d460be2a0eb3ce850a239e4bb525ed8
scarfacedotcom <peteradaaku@gmail.com>
2023-06-08 21:42:18 +0100

    added new functions

--- project/library.go
+++ project/library.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
+type Title string
+type Name string
+
+type LendAudit struct {
+	checkOut time.Time
+	checkIn  time.Time
+}
+
+type Member struct {
+	name  Name
+	books map[Title]LendAudit
+}
+
+type BookEntry struct {
+	total  int
+	lended int
+}
+
+type Library struct {
+	members map[Name]Member
+	books   map[Title]BookEntry
+}
+
+func printMemberAudit(member *Member) {
+	for title, audit := range member.books {
+		var returnTime string
+		if audit.checkIn.IsZero() {
+			returnTime = "[not returned yet]"
+		} else {
+			returnTime = audit.checkOut.String()
+		}
+		fmt.Println(member.name, ":", title, ":", audit.checkOut.String(), "through", returnTime)
+	}
+}
+
+func printMemberAudit(library *Library) {
+	for _, member := range library.members {
+		printMemberAudit(&member)
+	}
+
+}
+func printLibraryBooks(library *Library) {
+	fmt.Println()
+	for title, book := range library.books {
+		fmt.Println(title, "/ total:", book.total, "/ lended:", book.lended)
+	}
+	fmt.Println()
+
+}
 func main() {
 	fmt.Println("scar face")
 
